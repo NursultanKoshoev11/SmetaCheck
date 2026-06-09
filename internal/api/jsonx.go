3c7fd58506139aec7192e5d6ef33748ded0d623b
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-09 14:25:58 +0600

    Add JSON response helper

---
+++ internal/api/jsonx.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+ "encoding/json"
+ "net/http"
+)
+
+func JSON(w http.ResponseWriter,code int,v any){
+ w.Header().Set("Content-Type","application/json")
+ w.WriteHeader(code)
+ _=json.NewEncoder(w).Encode(v)
+}
