8bed1f8c44064070d19ba16a38e1f047ca3ad79f
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-09 15:56:16 +0600

    Add PostgreSQL storage repository

---
+++ internal/storage/repo.go
@@ -0,0 +1,11 @@
+package storage
+
+import(
+ "context"
+ "github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Store struct{db *pgxpool.Pool}
+func New(db *pgxpool.Pool)*Store{return &Store{db:db}}
+
+func (s *Store) Ping(ctx context.Context)error{return s.db.Ping(ctx)}
