2f441b25e2984a84b43429d99bbb26e0ddfe7c08
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-10 20:24:40 +0600

    Import detailed report styles

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles.css'
 import '../premium.css'
 import '../upload-modern.css'
+import '../report-detail.css'
 export default function App({Component,pageProps}){return <Component {...pageProps}/>}
