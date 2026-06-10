3478567f93a4cac395d470285b6dad5ad77bec84
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-10 22:29:32 +0600

    Import polished SaaS visual styles

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,4 +2,5 @@ import '../styles.css'
 import '../premium.css'
 import '../upload-modern.css'
 import '../report-detail.css'
+import '../saas-polish.css'
 export default function App({Component,pageProps}){return <Component {...pageProps}/>}
