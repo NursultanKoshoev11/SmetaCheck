8234899568ad175c85d1cb3e1b28bd6dea5c308a
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-11 16:09:09 +0600

    Remove duplicate CreateEstimate method

--- internal/storage/estimate.go
+++
@@ -1,8 +0,0 @@
-package storage
-
-import "context"
-
-func (s *Store) CreateEstimate(ctx context.Context,name,path string)error{
- _,err:=s.db.Exec(ctx,"insert into estimates(file_name,file_path,status) values($1,$2,$3)",name,path,"queued")
- return err
-}
