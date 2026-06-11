2c32dbb14acb68d56654033e02fa6a7a88616c79
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-11 13:11:11 +0600

    Load environment and start PostgreSQL API

---
+++ cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/NursultanKoshoev11/SmetaCheck/internal/api"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("environment file was not loaded: %v", err)
+	}
+	api.Run()
+}
