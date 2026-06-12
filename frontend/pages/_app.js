105761052288c6c056103caa7b3464197e06e155
Nursultan <102341503+NursultanKoshoev11@users.noreply.github.com>
2026-06-12 16:05:28 +0600

    Gate document pages behind explicit consent

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,7 +4,12 @@ import '../upload-modern.css'
 import '../report-detail.css'
 import '../saas-polish.css'
 import '../marketing.css'
+import {useRouter} from 'next/router'
+import DocumentConsentGate from '../components/DocumentConsentGate'
 
 export default function App({Component,pageProps}){
+  const router=useRouter()
+  const needsDocumentConsent=router.pathname==='/upload'||router.pathname==='/compare'
+  if(needsDocumentConsent)return <DocumentConsentGate><Component {...pageProps}/></DocumentConsentGate>
   return <Component {...pageProps}/>
 }
